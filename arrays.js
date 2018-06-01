9b07538bc7f628f1df0be4e741c8346aead2838c
Brenden Thornton <thorntonbrenden@gmail.com>
2018-06-01 00:57:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ['snicker', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
+  array = [element, ...array];
+  return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
@@ -10,7 +11,8 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  return [...array, element];
+  array = [...array, element];
+  return array;
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
@@ -28,7 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice(1);
+  array = array.slice(1);
+  return array;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
@@ -37,5 +40,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length - 1);
+  array = array.slice(0, array.length - 1);
+  return array;
 }
+
